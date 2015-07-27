7edd9edb27dddf07949d36c34efa47e418665bc6
Jesse Alford <jalford@pivotal.io>
2015-07-27 14:31:58 -0700

    Fixed automatic metronome logging option.

--- dora-sinatra/explore.rb
+++ dora-sinatra/explore.rb
@@ -1,5 +1,6 @@
 require 'rubygems'
 require 'sinatra'
+require 'logger'
 
 # Unique identifier for the app's lifecycle.
 #
@@ -9,13 +10,6 @@ ID = SecureRandom.uuid
 $stdout.sync = true
 $stderr.sync = true
 
-# This is handy for starting metronome logging on multiple instances without having to hit their API
-if ENV["METRONOME"]
-  hours = ENV["METRONOME"]
-  metronome(hours)
-end
-
-
 get '/' do
   host = ENV['VCAP_APP_HOST']
   port = ENV['VCAP_APP_PORT']
@@ -23,7 +17,7 @@ get '/' do
 end
 
 get '/metronome/:hours' do
-  metronome(params[:hours])
+  metronome(params[:hours], logger)
   # note: using curl or a browser to trigger this will not actually
   # you know, _load_ anything. This is, well, it's not normal,
   # but it is _expected._
@@ -98,10 +92,19 @@ get '/curl/:host/?:port?' do
   HTML
 end
 
-def metronome(:hours)
-  stop_time = Time.now + hours.to_i * 60 * 60
+def metronome(hours, logger)
+  stop_time = Time.now + hours.to_f * 60 * 60
   while Time.now < stop_time
     logger.info "Metronome Log from ID #{ID}, running as instance #{ENV['CF_INSTANCE_INDEX']}. Logging will end at #{stop_time}."
     sleep 1
   end
+end
+
+# This is handy for starting metronome logging on multiple instances without having to hit their API
+# Unfortunately, as currently implemented, it is incompatable with other uses of dora, such as haveing an API at all
+# In order to avoid being killed by the healthcheck, this option must be used in combination with --no-route
+# This makes Dora a worker, that does nothing but emit metronome logs.
+if ENV["METRONOME"]
+  hours = ENV["METRONOME"]
+  metronome(hours, Logger.new(STDERR))
 end
